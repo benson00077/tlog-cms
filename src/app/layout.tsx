0a6203522d88855628bef450a00902a0668c4b87
Benson <benson50622@gmail.com>
2023-10-24 15:51:14 +0800

    feat: side bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className='dark'>
+      <body className='bg-gray-50 dark:bg-gray-900'>
         <MyMultiLevelDropdown />
         {children}
       </body>
